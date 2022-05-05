0b317332bd0c3c5b348f8c8d0264edb88cf3aa8d
Iván Perdomo <ivan@perdomo.me>
2022-05-05 19:14:19 +0200

    Use `-T` instead of `--no-TTY` ¯\_(ツ)_/¯

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,8 @@
 set -eux
 
 docker-compose config
-docker-compose up --detach
-docker-compose exec --no-TTY client npm ci --no-progress
+docker-compose up -d
+docker-compose exec -T client npm ci --no-progress
 
 # Check for `node_modules` folder under `client`
 [[ -d "client/node_modules" ]] || { echo "node_modules not present"; exit 1; }
