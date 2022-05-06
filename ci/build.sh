1a14e9338e5b07ec3f1c67278427411ce9de6dec
Iván Perdomo <ivan@perdomo.me>
2022-05-06 15:29:57 +0200

    Minimal example with different type of `volume` definition

--- ci/build.sh
+++ ci/build.sh
@@ -4,9 +4,13 @@ set -eux
 
 docker-compose config
 docker-compose up -d
-docker-compose exec -T client npm ci --no-progress
 
-# Check for `node_modules` folder under `client`
-[[ -d "client/node_modules" ]] || { echo "node_modules not present"; exit 1; }
+docker-compose exec -T alpine touch /bind/bind.txt
+docker-compose exec -T alpine touch /default/default.txt
+docker-compose exec -T alpine touch /delegated/delegated.txt
+
+[[ -f "bind.txt" ]] || { echo "bind.txt not found"; exit 1; }
+[[ -f "default.txt" ]] || { echo "default.txt not found"; exit 1; }
+[[ -f "delegated.txt" ]] || { echo "delegated.txt not found"; exit 1; }
 
 echo "Done"
